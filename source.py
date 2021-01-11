cf84539b88b0170601e1a2371dba2965c4a1bbfb
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2021-01-11 07:09:22 +0200

    el function lly fatet byza 3mltelha comment

--- source.py
+++ source.py
@@ -111,14 +111,14 @@ for img in binary_notes_with_lines:
 # TODO Classification FINDING THE RHYTHM OF THE NOTES AND THE NUMBER OF THE NOTES //KARIM
 ##### PROCESSING EACH NOTE SEGMENT
 # show_images(notesImages)
-image = notesImages[13]  # [22]  # [17] # 28
+image = notesImages[21]  # [22]  # [17] # 28 # 10 # 6 # 21
 
-#### FINDING POSITION OF NOTE HEAD --> TOP = 0, BOTTOM = 1 and returning segmented images
-show_images([notesImages[13]])  # 4 #1 #14# 26 27 39 25
+
+show_images([notesImages[21]])  # 4 #1 #14# 26 27 39 25
 
 avgAreas = np.average(componentsAreas(notes))
-ratio = notesImages[13].shape[0] / notesImages[13].shape[1]
-area = notes[13].area
+ratio = notesImages[21].shape[0] / notesImages[21].shape[1]
+area = notes[21].area
 print("Area: ", area, " Average area:", avgAreas)
 print("ratio: ", ratio)
 if ratio > 1.4:
@@ -130,11 +130,16 @@ else:
 #### Removing Stems to count the number of notes
 V_staff_indices = find_verticalLines(image)
 print(V_staff_indices)
-image[:, V_staff_indices] = 0
-
-top_bottom, top_image, bot_image = classifyNotePositionInSegment(notesImages[1])
-print("Top or bottom:", top_bottom)
+#image[:, V_staff_indices] = 0
 stems_indices, stem_count = countStems(V_staff_indices)
+
+
+# s, t = get_references(image)
+image = binary_opening(image, np.ones((1, t + 4)))
+show_images([image])
+#### Find out where the notes are top or bottom
+top_bottom, top_image, bot_image = classifyNotePositionInSegment(image)
+print("Top or bottom:", top_bottom)
 print(stems_indices)
 if stem_count == 0:
     print("One Whole note")
@@ -203,190 +208,3 @@ elif stem_count == 1:
         # localMinimas.append((peaks[0] + peaks[1]) // 2)
         # localMinimas.append((peaks[1] + peaks[2]) // 2)
 
-    '''
-    ##### Segment the image based on
-    # for i in range(len(localMinimas)):
-
-    image = img_as_ubyte(image)
-    show_images([image])
-    edges = canny(image, sigma=2, low_threshold=10, high_threshold=50)
-    show_images([edges])
-
-    # Detect two radii
-    hough_radii = 0
-    if stacced_flag == 1:
-        hough_radii = np.arange(image.shape[1] // 4, image.shape[1] // 4 + 10, 1)
-    else:
-        hough_radii = np.arange(image.shape[1] // 2 - 10, image.shape[1] // 2, 5)
-    hough_res = hough_circle(edges, hough_radii)
-    print("hough radii", hough_radii)
-    # Select the most prominent 3 circles
-    accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii, total_num_peaks=numberOfPeaks)
-
-    # Draw them
-    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
-    image = color.gray2rgb(image)
-    for center_y, center_x, radius in zip(cy, cx, radii):
-        circy, circx = circle_perimeter(center_y, center_x, radius,
-                                        shape=image.shape)
-        image[circy, circx] = (220, 20, 20)
-
-    ax.imshow(image, cmap=plt.cm.gray)
-    plt.show()
-elif stem_count == 2:
-    print("Beamed note, we need to find the number of tails")
-'''
-'''
-# Load picture and detect edges
-referenceImg = gray_notes_with_lines[2].astype(float)
-image_gray = notesImages[2].astype(float)
-show_images([image_gray])
-image_gray *= 255.0
-print("Image gray shape : ", image_gray.shape)
-edges = canny(image_gray, sigma=2.0, low_threshold=0.55, high_threshold=0.8)
-
-# Perform a Hough Transform
-# The accuracy corresponds to the bin size of a major axis.
-# The value is chosen in order to get a single high accumulator.
-# The threshold eliminates low accumulators
-result = hough_ellipse(edges, accuracy=100, max_size=image_gray.shape[1])
-result.sort(order='accumulator')
-
-# Estimated parameters for the ellipse
-best = list(result[-1])
-yc, xc, a, b = [int(round(x)) for x in best[1:5]]
-
-orientation = best[5]
-
-# Draw the ellipse on the original image
-cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
-# print("Parameters of ellipse: ",cy, cx)
-# np.min(cy):np.max(cy), np.min(cx):np.max(cx)
-referenceImg[cy, cx] = 128.0
-# The_image[cy, cx, 1] = 0
-# The_image[cy, cx, 2] = 255
-# Draw the edge (white) and the resulting ellipse (red)
-edges = color.gray2rgb(img_as_ubyte(edges))
-edges[cy, cx] = (250, 0, 0)
-
-fig2, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4), sharex=True, sharey=True)
-
-ax1.set_title('Original picture')
-ax1.imshow(referenceImg)
-
-ax2.set_title('Edge (white) and result (red)')
-ax2.imshow(edges)
-
-plt.show()
-'''
-'''
-for Image in notesImages:
-    Image = thin(Image, 5)
-    show_images([Image])
-'''
-
-'''
-show_images(notesImages)
-sampleimg = notesImages[12]
-show_images([sampleimg])
-print(sampleimg.shape)
-shape = np.asarray(sampleimg.shape)
-shape = np.append(shape, 3)
-print(shape)
-wawa = np.zeros(shape)
-print("wawa:", wawa.shape)
-print(np.max(sampleimg))
-wawa[:, :, 0] = np.array(sampleimg * 255, dtype=np.uint8)
-wawa[:, :, 1] = np.array(sampleimg * 255, dtype=np.uint8)
-wawa[:, :, 2] = np.array(sampleimg * 255, dtype=np.uint8)
-
-wawa = np.negative(wawa)
-wawa = cv2.normalize(wawa, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-print(wawa)
-
-
-#img = np.zeros((int(sampleimg), 3))
-
-#img[:, :, 0] = sampleimg*255.0/255.0
-#img[:, :, 1] = sampleimg*255.0/255.0
-#img[:, :, 2] = sampleimg*255.0/255.0
-#print(img)
-
-
-img1 = cv2.imread('quarternote2.jpg')
-
-scale_percent = 100 # percent of original size
-width = int(img1.shape[1] * scale_percent / 100)
-height = int(img1.shape[0] * scale_percent / 100)
-dim = (width, height)
-# resize image
-img1 = cv2.resize(img1, dim, interpolation=cv2.INTER_AREA)
-print("yasta:", img1.shape)
-# img2 = cv2.imread(path)
-
-# convert images to grayscale
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-# img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
-
-########################
-# wawa = cv2.imread('cases/02.PNG')
-########################
-wawa = cv2.cvtColor(wawa, cv2.COLOR_BGR2GRAY)
-# img2 = cv2.cvtColor(rotated)
-# create SIFT object
-sift = cv2.xfeatures2d.SIFT_create()
-# detect SIFT features in both images
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(wawa, None)
-
-# des1 = des1.astype('float32')
-# des2 = des2.astype('float32')
-# FLANN parameters
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50)  # or pass empty dictionary
-
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-matches = flann.knnMatch(des1, des2, k=2)
-
-# Need to draw only good matches, so create a mask
-matchesMask = [[0, 0] for i in range(len(matches))]
-
-# ratio test as per Lowe's paper
-good = []
-MIN_MATCH_COUNT = 10
-for i, (m, n) in enumerate(matches):
-    if m.distance < 0.7 * n.distance:
-        matchesMask[i] = [1, 0]
-        good.append(m)
-print(np.sum(matchesMask))
-
-if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    matchesMask = mask.ravel().tolist()
-    print(M)
-    h, w = img1.shape
-    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-
-    dst = cv2.perspectiveTransform(pts, M)
-
-    wawa = cv2.polylines(wawa, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-
-else:
-    print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
-    matchesMask = None
-
-draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                   singlePointColor=None,
-                   matchesMask=matchesMask,  # draw only inliers
-                   flags=2)
-
-img3 = cv2.drawMatches(img1, kp1, wawa, kp2, good, None, **draw_params)
-
-plt.imshow(img3, 'gray'), plt.show()
-'''
-# show_images(notesImages)
